0bb3e58204950eaa1ebaf68669f921d28052cecc
kumarro <rohit.kumar@cdk.com>
2018-07-05 16:57:14 +0530

    cleanup

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -1,6 +1,3 @@
-import {ConfigDetails} from "../service/ConfigService";
-import {FileDetails} from "../service/FileService";
-
 export interface Command {
     doesHandle():boolean;
     handle(command:string):void;
